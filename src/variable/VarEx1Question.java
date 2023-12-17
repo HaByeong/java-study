5afeb4ad0ef4fa864b28945528406e6631eed6f9
HaByeong <121917642+HaByeong@users.noreply.github.com>
2023-12-17 23:49:41 +0900

    문제

---
+++ src/variable/VarEx1Question.java
@@ -0,0 +1,13 @@
+package variable;
+
+public class VarEx1Question {
+
+    public static void main(String[] args) {
+        int num1 = 4;
+        int num2 = 3;
+
+        System.out.println(num1+num2);
+        System.out.println(num1-num2);
+        System.out.println(num1*num2);
+    }
+}
---
+++ src/variable/VarEx3.java
@@ -0,0 +1,11 @@
+package variable;
+
+public class VarEx3 {
+    public static void main(String[] args) {
+        long longVar = 10000000000L;
+        boolean booleanVar = true;
+
+        System.out.println(longVar);
+        System.out.println(booleanVar);
+    }
+}
