7baebf749303fa9967cc04a5f31b4ae0ee6c3042
HaByeong <121917642+HaByeong@users.noreply.github.com>
2023-12-19 15:17:38 +0900

    if

---
+++ src/cond/If3.java
@@ -0,0 +1,11 @@
+package cond;
+
+public class If3 {
+    public static void main(String[] args) {
+        int age= 14;
+
+        if (age <= 7) {
+            System.out.println("미취학");
+        }
+    }
+}
