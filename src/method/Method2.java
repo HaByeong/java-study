281a2bae5b73f1d34681c9adc8577c0d2d482c25
HaByeong <121917642+HaByeong@users.noreply.github.com>
2023-12-26 16:29:08 +0900

    메서드

---
+++ src/method/Method2.java
@@ -0,0 +1,19 @@
+package method;
+
+public class Method2 {
+
+    public static void main(String[] args) {
+        printHeader();
+        printFooter();
+    }
+
+    public static void printHeader() {
+        System.out.println("= 프로그램을 시작합니다 =");
+        return;
+    }
+
+    public static void printFooter() {
+        System.out.println("= 프로그램을 종료합니다 =");
+        //return 생략 -> void는 자동으로 넣어줌
+    }
+}
